6f691424f717feb9beffa5407b1a1807046d8062
GoofyDoge <83433507+GoofyDoge@users.noreply.github.com>
2022-09-07 09:33:43 -0400

    Update course_info_parser.py
    Wrote doc string for function

--- src/course_info_parser.py
+++ src/course_info_parser.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 
 def get_course_info(file_name):
-    # df = pd.read_excel('input_files/ClassInfo.xlsx', sheet_name='Sheet1')
+    """ inputs file_name, reads excel file (specifically Sheet1),
+        returns pandas dataframe """
     df = pd.read_excel('src/input_files/' + file_name, sheet_name='Sheet1')
     return df
