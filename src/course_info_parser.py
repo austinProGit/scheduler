526efc91e50ae5bed65eb0ad3f9a662f5aebf3bc
austinProGit <97987573+austinProGit@users.noreply.github.com>
2022-09-09 15:51:21 -0400

    main

--- src/course_info_parser.py
+++ src/course_info_parser.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pandas as pd
 
 
@@ -7,13 +6,3 @@ def get_course_info(file_name):
         returns pandas dataframe """
     df = pd.read_excel('src/input_files/' + file_name, sheet_name='Sheet1')
     return df
-=======
-import pandas as pd
-
-
-def get_course_info(file_name):
-    """ inputs file_name, reads excel file (specifically Sheet1),
-        returns pandas dataframe """
-    df = pd.read_excel('src/input_files/' + file_name, sheet_name='Sheet1')
-    return df
->>>>>>> 4d843d10a2a378353d9295a6c0fde3c7968a14a5
