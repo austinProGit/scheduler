e7eb68665d21089415481a351635ecbb9e4e7363
GoofyDoge <83433507+GoofyDoge@users.noreply.github.com>
2022-09-17 19:05:13 -0400

    Update scheduler.py
    Wrapped in function. Added multi-semester scheduling. Returns list of lists, each inner list is one semester.

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,99 +1,97 @@
-# Scheduler file
-# WORK IN PROGRESS
+# Author: Vincent Miller
+# 12 September 2022
+"""
+TODO: CPSC 4000, only to be taken in last semester
+TODO: CPSC 3165, junior+ requirement
+TODO: CPSC 1301K, handle the k, temporarily removed the k from course_info.xlsx
+TODO: Create automatic semester types, Spring -> Sp, Summer -> Su, Fall -> Fa
+TODO: Create co-requisites check
+TODO: Deal with list sorting low level CYBR behind ALL CPSC courses, pointless if I get ALL courses through crawler?
+
+Over commented as usual for your reading pleasure.
+"""
 from courses_needed_parser import get_courses_needed
 from course_info_container import *
-import pandas as pd
 
 
-def check_availability(_course_id, _current_semester):
-    _availability = courses_info.get_availability_list(_course_id)
-    if _current_semester in _availability:
-        return True
-    else:
-        return False
+def schedule(courses_needed_file_name, course_info_file_name, max_courses):
+    """TODO: Write docstring for function"""
 
-
-def check_prerequisites(_course_id):
-    _prerequisites = courses_info.get_prereqs_list(_course_id)
-    print("PreReq: " + str(_prerequisites))
-    # if PreReq is in courses_needed or in current_semester, can't take course.
-    for prerequisite in _prerequisites:
-        if prerequisite in courses_needed or prerequisite in temporary_semester:
+    # helper functions
+    def check_availability(_course_id, _current_semester):
+        _availability = course_info.get_availability_list(_course_id)
+        if _current_semester in _availability:
+            return True
+        else:
             return False
-    return True
 
+    def check_prerequisites(_course_id):
+        _prerequisites = course_info.get_prereqs_list(_course_id)
+        # if prerequisite course is in courses_needed or in semester, can't take course yet.
+        for _prerequisite in _prerequisites:
+            if _prerequisite in courses_needed or _prerequisite in semester:
+                return False
+        return True
 
-# file names
-needed_file = 'Sample Input1.pdf'
-info_file = "ClassInfo.xlsx"
+    # get data being worked with
+    courses_needed = get_courses_needed(courses_needed_file_name)
+    course_info = Course_info_dataframe_container(load_course_info(course_info_file_name))
 
-# get data being worked with
-courses_needed = get_courses_needed(needed_file)
-courses_info = Course_info_dataframe_container(load_course_info('ClassInfo.xlsx'))
+    # sorting should speed up the scheduler, as lower level courses are more likely to be required sooner
+    # TODO: Low level CYBR courses will be pushed behind ALL CPSC courses, fix
+    courses_needed.sort()
 
-# create blank dataframe for final schedule
-schedule_df = pd.DataFrame(columns=['Spring 2023', 'Summer 2023', 'Fall 2023',
-                                    'Spring 2024', 'Summer 2024', 'Fall 2024',
-                                    'Spring 2025', 'Summer 2025', 'Fall 2025',
-                                    'Spring 2026', 'Summer 2026', 'Fall 2026'])
+    # create empty, final schedule, each semester list will be added to this
+    full_schedule = []
+    # temporary solution to deal with rotation of semester types
+    semester_types = ["Sp", "Su", "Fa"]
+    # loop through until courses_needed is empty
+    while len(courses_needed):
+        # loops through each semester type
+        # TODO: will need to adjust this loop with automatic semester type/date
+        for semester_type in semester_types:
+            # if courses_needed is empty, break out of semester_types loop
+            if not len(courses_needed):
+                break
 
-# only adding this course because it has two pre-reqs, Need one to test scheduler
-courses_needed.append("CPSC 3125")
+            # create empty semester, and set counters to 0
+            semester = []
+            current_courses_counter = 0  # counter to control max_courses
+            courses_counter = 0  # counter to control iteration through needed_courses
 
-# we need a max courses, and not a hard n courses per semester
-# I personally only had ONE class in a previous semester and could not take ANY other
-max_courses = 5
-# sorting should speed up the scheduler, as lower tier courses are more likely to be required sooner
-courses_needed.sort()
+            # loop to fill out semester, don't loop over max_courses,
+            # don't loop through courses_needed more than once, don't loop through courses_needed if empty
+            while current_courses_counter < max_courses \
+                    and courses_counter <= len(courses_needed) \
+                    and len(courses_needed):
 
-# only adding this course, to get a fail on my pre-reqs
-courses_needed.append("CPSC 2108")
-print(str(len(courses_needed)) + " Courses")
+                # get first course in list
+                course = courses_needed.pop(0)
+                courses_counter += 1
 
-# ************ single semester ********************
-temporary_semester = []
-current_courses = 0  # need to manually control this loop
-while current_courses < max_courses:
-    # TODO: Need to create something to check current semester and assign two letter semester
-    this_semester = 'Sp'
+                # check availability
+                available = check_availability(course, semester_type)
+                if not available:
+                    # course not available, add course back to courses_needed and skip rest of loop iteration
+                    courses_needed.append(course)
+                    continue
 
-    # get first course in list
-    course = courses_needed.pop(0)
-    print("Course: " + course)
+                # check prerequisites
+                prerequisites = check_prerequisites(course)
+                if not prerequisites:
+                    # prerequisite not met, add course back to courses_needed and skip rest of loop iteration
+                    courses_needed.append(course)
+                    continue
 
-    # check for non CPSC courses, these courses will throw errors when checking course_info
-    if course[:4] != "CPSC":
-        # slot course in semester
-        print("Added to semester")
-        temporary_semester.append(course)
-        current_courses += 1
-        continue
+                # check co-requisites
+                # TODO: create co-requisites check
 
-    # check availability
-    available = check_availability(course, this_semester)
-    if not available:
-        print("unavailable, skipped")
-        courses_needed.append(course)
-        continue
+                # add course in semester, increment counter
+                semester.append(course)
+                current_courses_counter += 1
 
-    # check prerequisites
-    # TODO: create co-requisites check
-    prerequisites = check_prerequisites(course)
-    if not prerequisites:
-        print("PreReq failed, skipped")
-        courses_needed.append(course)
-        continue
+            # add semester to full_schedule
+            full_schedule.append(semester)
 
-    # slot course in semester
-    print("Added to semester")
-    temporary_semester.append(course)
-    current_courses += 1
-
-print(temporary_semester)
-
-# first attempt, constant errors trying to go too broad at the start
-"""for semester in schedule_df:
-    # get course from courses needed
-    course = courses_needed.pop(0)
-
-    # check availability"""
+    # finished scheduling
+    return full_schedule
