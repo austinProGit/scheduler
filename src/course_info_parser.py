d17b99354b116dfd1129dadeb34d779230440507
GoofyDoge <83433507+GoofyDoge@users.noreply.github.com>
2022-09-17 19:00:17 -0400

    Update course_info_parser.py
    Added comments, and another relative file path line to pick from

--- src/course_info_parser.py
+++ src/course_info_parser.py
@@ -1,8 +1,11 @@
+# Author: Vincent Miller
+# Date: 7 September 2022
 import pandas as pd
 
 
 def get_course_info(file_name):
-    """ inputs file_name, reads excel file (specifically Sheet1),
+    """ inputs file_name, reads Excel file_name (specifically Sheet1),
         returns pandas dataframe """
-    df = pd.read_excel('src/input_files/' + file_name, sheet_name='Sheet1')
+    df = pd.read_excel('src/input_files/' + file_name, sheet_name='Sheet1') # TODO: file_name path is lame, pick the one that works for you
+    # df = pd.read_excel('input_files/' + file_name, sheet_name='Sheet1')
     return df
