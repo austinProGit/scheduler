368dcf209ac8fa534bb982fb8fcda278185381b2
GoofyDoge <83433507+GoofyDoge@users.noreply.github.com>
2022-09-17 19:02:26 -0400

    Update courses_needed_parser.py
    Updated regex pattern for CSPC and CYBR courses

--- src/courses_needed_parser.py
+++ src/courses_needed_parser.py
@@ -1,6 +1,9 @@
+# Author: Vincent Miller
+# Date: 31 August 2022
 """
 TODO: fix one of the dataframes from tabula gets included as column title data, needs to be data
 TODO: find solution for electives, Ex: 6 Credits in CPSC 3@ or 4@ or 5@
+TODO: decide on regex pattern
 """
 import pandas as pd
 import tabula
@@ -8,31 +11,33 @@ import re
 
 
 def get_courses_needed(file_name):
-    """Inputs file name as string, reads pdf tables into list of dataframes per table,
-       merges list of dataframes into single dataframe, parses dataframe for course id's,
-       adds course id's to list (can change to set or other), and returns list
-       A lot of this code is over commented, but I want to make sure all of you understand it."""
-    # create blank dataframe
-    courses_needed_df = pd.DataFrame()
+    """Inputs file_name name as string, reads pdf tables into list of dataframes per table,
+       merges list of dataframes into single dataframe, parses dataframe for CPSC and CYBR course id's,
+       adds course id's to list, and returns list"""
+
     # read and import tables from pdf, returns as list of dataframes
-    courses_needed_df_list = tabula.read_pdf('src/input_files/' + file_name, pages='all')
-    # regex shenanigans
-    # [A-Z]{4} looks for 4 capital letters, \s{1} looks for 1 space, \d{4} looks for 4 digits/numbers
-    # pattern is used to find course id, Ex: CPSC 1301, MATH 2125, etc.
-    course_pattern = r'[A-Z]{4}\s{1}\d{4}'
-    courses_needed_list = list()
+    # TODO: why is file_name relative path lame, figure out and fix later
+    # courses_needed_df_list = tabula.read_pdf('src/input_files/' + file_name, pages='all')
+    courses_needed_df_list = tabula.read_pdf('input_files/' + file_name, pages='all')
 
     # merge list of dataframes into one dataframe
+    courses_needed_df = pd.DataFrame()
     for i in range(len(courses_needed_df_list)):
         courses_needed_df = pd.concat([courses_needed_df, courses_needed_df_list[i]], axis=0, ignore_index=True)
 
-    # re.findall(pattern, string), searches and returns ALL patterns as list of string or tuple
-    # re.search(pattern, string), searches for FIRST match of pattern and returns as match object
+    # regex pattern is used to find course id, Ex: CPSC 1301, MATH 2125, etc.
+    # course_pattern = r'[A-Z]{4}\s{1}\d{4}'  # pattern for all courses
+    course_pattern2 = r'CPSC\s{1}\d{4}'  # pattern for CPSC courses
+    course_pattern3 = r'CYBR\s{1}\d{4}'  # pattern for CYBR courses
 
     # search dataframe for course id pattern, adds to list
+    courses_needed_list = list()
     for element in courses_needed_df['Unnamed: 0']:
-        match = re.search(course_pattern, str(element))
-        if match:
-            courses_needed_list.append(match.group())
+        cspc_match = re.search(course_pattern2, str(element))
+        cybr_match = re.search(course_pattern3, str(element))
+        if cspc_match:
+            courses_needed_list.append(cspc_match.group())
+        elif cybr_match:
+            courses_needed_list.append(cybr_match.group())
 
     return courses_needed_list
