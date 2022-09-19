bed635fe522b6dedfd8f38ec76cc6d8aea68fe02
GoofyDoge <83433507+GoofyDoge@users.noreply.github.com>
2022-09-19 14:08:14 -0400

    Add files via upload
    Added my WIP for the catalog parser/course info generator.

---
+++ src/catalog_parser.py
@@ -0,0 +1,67 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+
+
+def extract_prerequisites():
+    # course_body[0] is description, [1] is prerequisites OR repeatability [2] is repeatability
+    # TODO: handle restrictions ('p', class_='cls noindent')
+    _prerequisites = ''  # create blank string for prerequisites
+    try:  # course_body list varies in size from 1 to 3
+        course_pattern = r'[A-Z]{4}\s{1}\d{4}'  # pattern for all courses
+        print(course_body[1].text)
+        # sometimes the hex value, \xa0, is in between course id's. Ex: MATH\xa01113
+        _prerequisites = re.findall(course_pattern, str(course_body[1].text).replace("\xa0", " "))
+        # remove duplicates by converting to set
+        _prerequisites = [*set(_prerequisites)]
+
+        # these courses are either typo's or out of date and shouldn't have been in the catalog
+        bad_courses = ['CSCI 1301', 'CYBR 2106', 'CPSC 5115']  # typo's in data, or out of date
+        for element in bad_courses:
+            if element in _prerequisites:
+                _prerequisites.remove(element)
+        _prerequisites.sort()
+        # convert back to string, as to not break Lew's container
+        _prerequisites = ', '.join(_prerequisites)
+        print(_prerequisites)
+    except IndexError:  # course has no prerequisites or readability, only description
+        pass
+
+    return _prerequisites
+
+
+# open url, load into bs4 object
+url = "https://catalog.columbusstate.edu/course-descriptions/cpsc/"
+html = urlopen(url).read()
+soup = BeautifulSoup(html, features="html.parser")
+
+# extract all course block data
+all_course_blocks = soup.find_all('div', class_='courseblock')
+
+# create dataframe to hold course info
+course_info_df = pd.DataFrame(columns=['ID', 'name', 'hours', 'availability', 'prerequisites', 'co-requisites',
+                                       'recommended', 'isElective', 'restrictions', 'note'])
+
+# loop to add course data to dataframe
+for index in range(len(all_course_blocks)):
+    # extract current course header and body html data
+    course_header = all_course_blocks[index].find_all('div', class_='cols noindent')
+    course_body = all_course_blocks[index].find_all('p', class_='courseblockextra noindent')
+    # extract course id
+    course_id = course_header[0].find('span', class_='text col-3 detail-code margin--tiny text--semibold text--huge')
+    print(course_id.text)
+    # extract course name
+    course_name = course_header[0].find('span', class_='text col-3 detail-title margin--tiny text--bold text--huge')
+    # extract course hours TODO: only need credit hours, remove physical class and lab hours
+    course_hours = course_header[0].find('span', class_='text detail-coursehours text--bold text--huge')
+    # extract prerequisites
+    prerequisites = extract_prerequisites()
+    # add course to dataframe
+    course_info_df.loc[len(course_info_df.index)] = [course_id.text, course_name.text, course_hours.text, '',
+                                                     prerequisites, '', '', 0, '', '']
+
+# export to excel, and save to specific sheets TODO: create and add CYBR sheet
+excel = pd.ExcelWriter('Course Info.xlsx')
+course_info_df.to_excel(excel, 'CPSC')
+excel.save()
