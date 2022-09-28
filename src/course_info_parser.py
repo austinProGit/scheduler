731f4015a1acc151d640010ffe9019b62f437692
GoofyDoge <83433507+GoofyDoge@users.noreply.github.com>
2022-09-28 17:01:37 -0400

    Update course_info_parser.py

--- src/course_info_parser.py
+++ src/course_info_parser.py
@@ -2,9 +2,6 @@
 # Date: 7 September 2022
 import pandas as pd
 
-# MERINO: are we even using this?
-# V: You specifically demanded this file. We modeled to have this file.
-
 
 def get_course_info(file_name):
     """ inputs file_name, reads Excel file_name (specifically Sheet1),
