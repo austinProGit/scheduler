4b6f20a99170c275f97142032375c56f33db8200
GoofyDoge <83433507+GoofyDoge@users.noreply.github.com>
2022-09-28 15:00:16 -0400

    Update course_info_parser.py
    Cleaned up.

--- src/course_info_parser.py
+++ src/course_info_parser.py
@@ -3,10 +3,11 @@
 import pandas as pd
 
 # MERINO: are we even using this?
+# V: You specifically demanded this file. We modeled to have this file.
+
+
 def get_course_info(file_name):
     """ inputs file_name, reads Excel file_name (specifically Sheet1),
         returns pandas dataframe """
-    # MERINO: removed "input_files/" from file_name
-    df = pd.read_excel(file_name, sheet_name='Sheet1') # TODO: file_name path is lame, pick the one that works for you
-    # df = pd.read_excel('input_files/' + file_name, sheet_name='Sheet1')
+    df = pd.read_excel(file_name, sheet_name='Sheet1')
     return df
