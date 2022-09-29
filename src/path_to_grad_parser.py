7b5d2e9029d4964e05bef94a00288fad96b48977
GoofyDoge <83433507+GoofyDoge@users.noreply.github.com>
2022-09-29 13:26:32 -0400

    Update path_to_grad_parser.py
    Cleaned up and commented code.

--- src/path_to_grad_parser.py
+++ src/path_to_grad_parser.py
@@ -1,28 +1,16 @@
-# Author: Vincent Miller
+# Author: Vincent Miller, Thomas Merino
 # Date: 28 September 2022
 
 import pandas as pd
 import re
 
-ROW_RANGES = [
-    (2, 9),
-    (11, 18),
-    (20, 27),
-    (29, 36),
-    (38, 45)
-]
 
 def parse_path_to_grad(file_name):
-    df = pd.read_excel(file_name, sheet_name='Sheet1')
-
-#    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-#        print(df)
-
-    schedule = list()
-    course_pattern = r'[A-Z]{4}\s{1}\d{4}'
-    # TODO: CPSC elective patternn (is this even necessary given the application?)
-
+    """reads file name/path, parses courses out by semester, 
+       returns schedule as list of lists, inner list is one semester"""
+    
     def parse_semester(_start, _stop, _column):
+        """helper function, parses courses from one semester"""
         _semester = []
         for i in range(_start, _stop):
             _course = df[_column][i]
@@ -31,17 +19,20 @@ def parse_path_to_grad(file_name):
                 _semester.append(_course.group())
         return _semester
     
+    # courses are in these ranges in dataframe
+    ROW_RANGES = [(2, 9),
+                  (11, 18),
+                  (20, 27),
+                  (29, 36),
+                  (38, 45)]
+    
+    df = pd.read_excel(file_name, sheet_name='Sheet1')
+    schedule = list()
+    course_pattern = r'[A-Z]{4}\s{1}\d{4}'
+
+    # loops through to extract schedle, Unnamed: 0 = Fa, Unnamed: 2 = Sp, Unnamed: 4 = Su
     for (start, end) in ROW_RANGES:
         for column in ['Unnamed: 0', 'Unnamed: 2', 'Unnamed: 4']:
             schedule.append(parse_semester(start, end, column))
             
     return schedule
-
-# first semester, all FALL is in this column
-
-
-
-if __name__ == '__main__':
-    schedule = parse_path_to_grad("input_files/Path to Graduation3.xlsx")
-    for semester in schedule:
-        print(semester)
