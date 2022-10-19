332b3f0750632cc841711ce6237bb62d1962fcf7
austinProGit <97987573+austinProGit@users.noreply.github.com>
2022-10-19 17:26:46 -0400

    Course Info, Excused Prereqs

--- src/program_generated_validator.py
+++ src/program_generated_validator.py
@@ -25,44 +25,37 @@ def validate_course_path(container):
     # Get all the courseIDs as a list of IDs
     courseids = container.get_courseIDs()
 
-    # List to store all invalid prereqs
-    invalid_prereq_list = set()
+    # Get all the valid prereqs not listed as courses
+    excused_prereqs = container.get_excused_prereqs()
 
 # We want the program to store a list of invalid prereqs. 
 # We need to create an errorList[] that stores all of the errors found during execution. (a list of strings)
 
-    for course in courseids:                           # Iterate through the list of course IDs
-        prereqs = container.get_prereqs(course)        # Get the prereqs for each course ID
-        for prereq in prereqs:                         # For each prereq for a given course
-            if prereq in courseids:                    # Check if the prereq is found in the list of courses
+    for course in courseids:                                    # Iterate through the list of course IDs
+        prereqs = container.get_prereqs(course)                 # Get the prereqs for each course ID
+        for prereq in prereqs:                                  # For each prereq for a given course
+            if prereq in courseids or prereq in excused_prereqs:  # Check if the prereq is found in the list of courses
                 if is_valid_course_path:
-                    G.add_edge(prereq, course)             # Add a directed edge from the prereq to the course
+                    G.add_edge(prereq, course)                  # Add a directed edge from the prereq to the course
             else:
-                is_valid_course_path = False           # If invalid course is found, course path is invalid
-                invalid_prereq_list.add(prereq)
-                # raise InvalidCourseError(f'Invalid course {prereq} has been found.')
+                is_valid_course_path = False                    # If invalid course is found, course path is invalid
+                excused_prereqs.append(prereq)
     
     if not is_valid_course_path:
-        raise InvalidCourseError(f'Invalid course(s) {", ".join(invalid_prereq_list) } has been found.')
+        raise InvalidCourseError(f'Invalid course(s) {", ".join(excused_prereqs) } has been found.')
     
-    cycle_list = []
+    cycles_list = []
 
-    cycle_list = sorted(nx.simple_cycles(G))
-    # note from thomas: "->".join(cycles) + "->" + cycles[-1]
+    cycles_list = sorted(nx.simple_cycles(G))
+    cycles_strings = []
+    for cycle in cycles_list:
+        cycle.reverse()
+        new_first_index = next((cycle.index(id) for id in courseids if id in cycle))
+        cycle = cycle[new_first_index:] + cycle[:new_first_index]
+        cycle.append(cycle[0])
+        cycles_strings.append('-->'.join(cycle))
 
-    if len(cycle_list) > 0:
-        raise NonDAGCourseInfoError(f'Cycle list: {cycle_list}')
-
-    # Check to see if the created DAG has cycles; return result
-    # is_valid_course_path = is_valid_course_path and nx.is_directed_acyclic_graph(G)
-    # print(is_valid_course_path)
-    # for edge in G.edges:
-    #     print(edge)
-    
-    # MERINO: added this check with raise if test failed (note that the return value is kind of useless then; maybe refactor)
-    # if not is_valid_course_path:
-    #     raise NonDAGCourseInfoError('Invalid course prerequisite cycle has been found.')
-    
-    # return is_valid_course_path
+    if len(cycles_list) > 0:
+        raise NonDAGCourseInfoError(f'Cycle list: {cycles_strings}')
     
         
