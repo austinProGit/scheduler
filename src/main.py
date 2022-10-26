b5f6b94cec0eb9c1f95f831af7d8b5232e047b9e
GoofyDoge <83433507+GoofyDoge@users.noreply.github.com>
2022-10-26 17:08:58 -0400

    Update main.py
    I forgot to delete my batch_processing stuff.

--- src/main.py
+++ src/main.py
@@ -3,26 +3,7 @@
 # Description: Main file for our program, runs smart planner controller
 
 from program_driver import SmartPlannerController
-
-# TODO: Move this to demo code file until Thomas understands how it works and implements
-# imports required to run batch_process
-from batch_process import batch_process
-from course_info_container import CourseInfoContainer
-from course_info_parser import get_course_info
-
-if __name__ == "__main__":
-    # get course_info container
-    course_info = CourseInfoContainer(get_course_info("input_files/Course Info.xlsx"))
-    # get required directory paths
-    input_path = "batch_process_input/"
-    output_path = "batch_process_schedules/"
-    template_path = "input_files/Path To Graduation Y.xlsx"
-
-    batch_process(input_path, output_path, template_path, course_info)
-
-""" COMMENTED OUT BELOW IS ORIGINAL CODE
     planner = SmartPlannerController()
 
     if planner.has_interface():
        while planner.run_top_interface(): pass
-"""
