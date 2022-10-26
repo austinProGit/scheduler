5709819e168e8e36e54cfd7d1d768f36ac5c52a8
GoofyDoge <83433507+GoofyDoge@users.noreply.github.com>
2022-10-26 10:24:48 -0400

    Finished batch_processing TODOs
    Finished TODO's for batch processing

--- src/batch_process.py
+++ src/batch_process.py
@@ -9,38 +9,47 @@ from itertools import repeat
 from pathlib import Path
 import os
 
-# TODO: File names as ID_Hours.pdf
-# TODO: Wrong git account
-
 
 def batch_process(input_path, output_path, template_path, course_info):
-    # TODO: DOCSTRING
+    """function performs multiprocessing using ProcessPoolExecutor
+        which will create as many processes as your computer has cores.
+       inputs: input_path is folder containing degreeworks
+               output_path is the folder for the output schedules
+               template_path for Path to Grad template
+               course_info container
+       outputs: places schedules into the output_path folder"""
     # get list of files to process
     files_to_process = os.listdir(input_path)
     # perform multiprocessing, How To: map(function, *args), repeat() is for non list arguments
     with ProcessPoolExecutor() as executor:
-        executor.map(full_schedule, files_to_process, repeat(output_path),
+        executor.map(full_schedule, repeat(input_path), files_to_process, repeat(output_path),
                      repeat(template_path), repeat(course_info))
 
 
-def full_schedule(courses_needed_filename, output_path, template_path, course_info):
-    # TODO: DOCSTRING
+def full_schedule(input_path, courses_needed_filename, output_path, template_path, course_info):
+    """function will produce schedule from start to finish
+       inputs: input_path is the folder containing all degreeworks files
+               file_name,
+               output_path for where to store schedules,
+               template_path for the Path to Grad template,
+               course_info container
+       outputs: places schedule in the output path folder"""
+    print(f"Starting schedule for {courses_needed_filename}", input_path)
     # read courses needed
-    courses_needed = get_courses_needed("batch_process_input/" + courses_needed_filename)
+    courses_needed = get_courses_needed(input_path + courses_needed_filename)
 
-    # TODO: strip hours from file name
     # get hours per semester
-    semester_hours = 15
+    semester_hours = int(courses_needed_filename[10:12])
 
     # run scheduler
-    # TODO: Scheduler with constructor to accept multiple configures, MUST use setters currently
     scheduler = Scheduler()
     scheduler.configure_course_info(course_info)
     scheduler.configure_courses_needed(courses_needed)
     scheduler.configure_hours_per_semester(semester_hours)
     schedule = scheduler.generate_schedule()
 
-    # TODO: make correct output names, student_ID.xlsx
     output_file_path = output_path + Path(courses_needed_filename).stem + ".xlsx"
+    print(f"Exporting {courses_needed_filename} to excel")
+    # TODO: formatter is broken?
     # export to excel
     excel_formatter(template_path, output_file_path, schedule, course_info)
