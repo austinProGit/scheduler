8abd4e6265f421ee1809e57facbd934626f976e2
GoofyDoge <83433507+GoofyDoge@users.noreply.github.com>
2022-10-26 17:12:07 -0400

    Update course_info_parser.py
    Sheet1 was changed to CPSC

--- src/course_info_parser.py
+++ src/course_info_parser.py
@@ -6,5 +6,5 @@ import pandas as pd
 def get_course_info(file_name):
     """ inputs file_name, reads Excel file_name (specifically Sheet1),
         returns pandas dataframe """
-    df = pd.read_excel(file_name, sheet_name='Sheet1')
+    df = pd.read_excel(file_name, sheet_name='CPSC')
     return df
