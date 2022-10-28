da392cd56677e8bc1d982d8417cf8df537e2cf9c
austinProGit <97987573+austinProGit@users.noreply.github.com>
2022-10-28 14:55:26 -0400

    added check for availability to user_submitted_val

--- src/user_submitted_validator.py
+++ src/user_submitted_validator.py
@@ -1,13 +1,24 @@
-# Author: Austin Lee and Max Lewis
+# Author: Austin Lee, Max Lewis, and Thomas Merino
 # 9/28/22
 # CPSC 4175 Group Project
 
+SEMESTER_TYPE_PREDECESSOR = {'Fa': 'Su', 'Su': 'Sp', 'Sp': 'Fa'}    # Translation map from semester K to the next
+USER_READABLE_SEMESTER_DESCRIPTION = {'Fa': 'Fall', 'Sp': 'Spring', 'Su': 'Summer'}
+
 def validate_user_submitted_path(container, schedule):
+    '''This assumes the last semster in the list of list is Summer.'''
     processed_list = []
     err_list = []
     err_str = ''
+    current_semester = 'Su'
+    
     for semester in reversed(schedule):
+
         for course in semester:
+            if current_semester not in container.get_availability(course):
+                err_str = (f'{course} taken during the {USER_READABLE_SEMESTER_DESCRIPTION[current_semester]} term (not available).')
+                err_list.append(err_str)
+
             course_coreqs = container.get_coreqs(course)
             for coreq in course_coreqs:
                 if coreq in processed_list:
@@ -27,6 +38,11 @@ def validate_user_submitted_path(container, schedule):
                 if prereq in processed_list:
                     err_str = (f'{course} taken before/during {prereq}.')
                     err_list.append(err_str)
+
+        # Update the semester term
+        current_semester = SEMESTER_TYPE_PREDECESSOR[current_semester]
+
+    # print(err_list)   
     return err_list
 
     # throw the semester's courses in big list
