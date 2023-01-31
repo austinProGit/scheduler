00a4d52a29d42ff1367d3b4f8b411decde69c5c1
austinProGit <97987573+austinProGit@users.noreply.github.com>
2023-01-30 19:42:29 -0500

    get_courses_needed(), updated req.txt rem Tabula

--- src/courses_needed_parser.py
+++ src/courses_needed_parser.py
@@ -1,98 +1,98 @@
-# Author: Vincent Miller
-# Date: 31 August 2022
-# Updated: 28 September 2022, Added exceptions for MATH 1113, 2125, 5125, and electives
+# Austin Lee 1/23/2023
+# CPSC 4175 Project
 
 from alias_module import get_latest_id
-
-import pandas as pd
-import tabula
+# from courses_needed_container import CoursesNeededContainer, \
+#     DeliverableCourse, CourseProtocol, CourseInserter, ExhaustiveNode, \
+#     ShallowCountBasedNode, DeepCountBasedNode, DeepCreditBasedNode
 import re
+from pypdf import PdfReader
 
-def get_courses_needed(file_name):
-    """Inputs file_name name as string, reads pdf tables into list of dataframes per table,
-       merges list of dataframes into single dataframe, parses dataframe for CPSC and CYBR course id's,
-       adds course id's to list, and returns list"""
+def get_courses_needed(file_name): 
+    """
+    Gets the needed courses from the Degree Works.
 
-    # read and import tables from pdf, returns as list of dataframes
-    courses_needed_df_list = tabula.read_pdf(file_name, guess=False, pages='all', silent=False)
+    Parameters
+    ----------
+    arg1 : file_name
+        File name of the DegreeWorks being parsed
+
+    Returns
+    -------
+    courses_needed_container
+        This container contains a recursively-defined tree data structure 
+        that holds the relationships between all needed courses found in
+        a DegreeWorks pdf.
+
+    """
+    with open(file_name, 'rb') as pdf_file:
+        pdf_reader = PdfReader(pdf_file)
+        required_courses = []
+        still_needed_chunks_list = [] # Holds all unprocessed/filtered chunks
+        document_string = '' # Holds the entire parsed pdf text
+        for page in range(len(pdf_reader.pages)):
+            page_text = pdf_reader.pages[page].extract_text()
+            document_string += page_text
+        start = document_string.find("Still Needed:") # Used to initiate every chunk
+        while start != -1:
+            end = document_string.find("Still Needed:", start + 1) # Searches for next instance of phrase
+            if end != -1:
+                still_needed_chunks_list.append(document_string[start:end]) 
+                start = end
+            else: # This case accounts for the final instance of the phrase
+                still_needed_chunks_list.append(document_string[start:document_string.find('Fallthrough', start + 1)])
+                break
+        currently_in_logical_block = False # Used to track if currently parsing a multi-line logical block
+        logically_grouped_list = [] # Holds the chunks that contain all logical groupings of needed courses
+        logical_chunk_string = '' # Holds the current chunk being parsed
+        for chunk in still_needed_chunks_list:
+            if 'of the following:' in chunk or ') or' in chunk: # Indicates the start or continuance of a logical chunk
+                currently_in_logical_block = True
+            elif currently_in_logical_block:
+                    currently_in_logical_block = False
+                    logical_chunk_string += chunk # Appends last portion of a logical chunk
+                    logically_grouped_list.append(logical_chunk_string)
+            if currently_in_logical_block:
+                logical_chunk_string += chunk
+            else:
+                logically_grouped_list.append(chunk) # Appends simple chunks
+        filtered_list = [] # Holds the filtered chunks
     
-    # merge list of dataframes into one dataframe
-    courses_needed_df = pd.DataFrame()
-    for i in range(len(courses_needed_df_list)):
-        courses_needed_df = pd.concat([courses_needed_df, courses_needed_df_list[i]], axis=0, ignore_index=True)
+    return required_courses # Does not currently meet final function signature
 
-    # regex pattern is used to find course id, Ex: CPSC 1301, MATH 2125, etc.
-    # course_pattern = r'[A-Z]{4}\s{1}\d{4}'  # pattern for all courses
-    cpsc_pattern = r'CPSC\s{1}\d{4}'
-    cybr_pattern = r'CYBR\s{1}\d{4}'
-    pre_cal_pattern = r'MATH 1113'
-    intro_discrete_pattern = r'MATH 2125'
-    discrete_pattern = r'MATH 5125'
-    elective_pattern1 = r'6 Credits in CPSC 3@'
-    elective_pattern2 = r'3 Credits in CPSC 3@'
+if __name__ == "__main__":
+    print(get_courses_needed('./src/input_files/degreeworks1.pdf'))      
+
+    # TODO: 
+        # filter out filler chunks ex: Outcomes Assessment Exam
+            # no XXXX XXXX or XXXX X@
+        # filter out courses taken information. Ex:
+            # find Season Year, backtrack to last mention of XXXX, remove from chunk
+        # filter out the case where 'Select one as a pre-requesite for STAT 3127'
+            # comes before the still needed
+        # filter out the 'still needed:'
+        # filter out anything that has no XXXX XXXX or XXXX X@
+
+    # TODO:
+        # Create a filtered_list that has all garbage removed from each chunk. Junk chunks are not added to filtered_list
+
+    # TODO:
+        # Creat a tokenized_list that has all chunks normalized into predictable logical structures
+
+    # Cases:
+        # Case 1: 'POLS 1101'
+        # Case 2: 'CPSC 4157 or 5157'
+        # Case 3: 'POLS 1201 or CPSC 1234'
+        # Case 4: 'POLS 1201 and 
+        # Case 5: 'HIST 2111, 2112 or GA History Exam'
+        # We need to load the course info dtb before handling case 6:
+        # Case 6: 'CPSC 3@ or 4@ or 5@'
+        # We need to handle electives
+        # Case 7: Except Case: 1 Credit in @ 1@ or 2@ or 3@ or 4@ or 5@U Except ENGL 1101 or 1102*
+
+    # Useful code:
+        # matches = re.findall(r'[A-Z]{4}\s{1}\d{4}', lines[i][lines[i].index('Still Needed:') + 12:])
+
+
     
     
-    # search dataframe for course id pattern, adds to list
-    courses_needed_list = list()
-    for col_name, _ in courses_needed_df.iteritems():
-        # check column names for pattern
-        cspc_col_match = re.search(cpsc_pattern, str(col_name))
-        cybr_col_match = re.search(cybr_pattern, str(col_name))
-        pre_cal_col_match = re.search(pre_cal_pattern, str(col_name))
-        intro_discrete_col_match = re.search(intro_discrete_pattern, str(col_name))
-        discrete_col_match = re.search(discrete_pattern, str(col_name))
-        elective_col_match1 = re.search(elective_pattern1, str(col_name))
-        elective_col_match2 = re.search(elective_pattern2, str(col_name))
-
-        # add to list if found
-        if cspc_col_match:
-            courses_needed_list.append(get_latest_id(cspc_col_match.group()))
-        elif cybr_col_match:
-            courses_needed_list.append(get_latest_id(cybr_col_match.group()))
-        elif pre_cal_col_match:
-            courses_needed_list.append(get_latest_id(pre_cal_col_match.group()))
-        elif intro_discrete_col_match:
-            courses_needed_list.append(get_latest_id(intro_discrete_col_match.group()))
-        elif discrete_col_match:
-            courses_needed_list.append(get_latest_id(discrete_col_match.group()))
-        elif elective_col_match1:
-            courses_needed_list.append('CPSC 3XXX')
-            courses_needed_list.append('CPSC 3XXX')
-        elif elective_col_match2:
-            courses_needed_list.append('CPSC 3XXX')
-    
-        for element in courses_needed_df[col_name]:
-            # check data in column for pattern
-            cspc_match = re.search(cpsc_pattern, str(element))
-            cybr_match = re.search(cybr_pattern, str(element))
-            pre_cal_match = re.search(pre_cal_pattern, str(element))
-            intro_discrete_match = re.search(intro_discrete_pattern, str(element))
-            discrete_match = re.search(discrete_pattern, str(element))
-            elective_match1 = re.search(elective_pattern1, str(element))
-            elective_match2 = re.search(elective_pattern2, str(element))
-
-            # add to list if found
-            if cspc_match:
-                courses_needed_list.append(get_latest_id(cspc_match.group()))
-            elif cybr_match:
-                courses_needed_list.append(get_latest_id(cybr_match.group()))
-            elif pre_cal_match:
-                courses_needed_list.append(get_latest_id(pre_cal_match.group()))
-            elif intro_discrete_match:
-                courses_needed_list.append(get_latest_id(intro_discrete_match.group()))
-            elif discrete_match:
-                courses_needed_list.append(get_latest_id(discrete_match.group()))
-            elif elective_match1:
-                courses_needed_list.append('CPSC 3XXX')
-                courses_needed_list.append('CPSC 3XXX')
-            elif elective_match2:
-                courses_needed_list.append('CPSC 3XXX')
-
-    # sample inputs have CPSC 4115 mislabeled as CPSC 5115
-    if 'CPSC 5115' in courses_needed_list:
-        courses_needed_list.remove('CPSC 5115')
-        if 'CPSC 4115' not in courses_needed_list:
-            courses_needed_list.append('CPSC 4115')
-
-    courses_needed_list.sort()
-    return courses_needed_list
