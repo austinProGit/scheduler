fc6454e283a151b2b5f8bd8d3d8e8e5739092a5f
Bixbite36 <109225430+Bixbite36@users.noreply.github.com>
2023-04-24 10:59:39 -0400

    Intermediate work

--- src/help_interface.py
+++ src/help_interface.py
@@ -2,48 +2,59 @@
 # 2/20/23
 # CPSC 4175 Group Project
 
+
+# The following are imported for type annotations
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from typing import Optional, Any
+    from io import TextIOWrapper
+
+
 from menu_interface_base import GeneralInterface
 from difflib import SequenceMatcher
 import os # TODO: this is only used for one thing, and it not good (see below)
 
-HELP_FILENAME = 'help.html' # The filename of the program's help documentation (this must be kept in a specific format)
-HELP_TERMINATOR = '</body>' # The string that ends help documentation parsing
-HELP_PRE_START = '<h1>' # The string that will start help documentation parsing
-HELP_HEADER_START_TOKEN = '<h3>' # The string that will start header string
-HELP_HEADER_END_TOKEN = '</h3>' # The string that will end header string
-HELP_TAG_START_TOKEN = '<!--' # The string that will start tokens string
-HELP_TAG_END_TOKEN = '-->' # The string that will end tokens string
-HELP_PARAGRAPH_START_TOKEN = '<p>' # The string that will start paragraph string
-HELP_PARAGRAPH_END_TOKEN = '</p>' # The string that will end paragraph string
+HELP_FILENAME: str = 'help.html' # The filename of the program's help documentation (this must be kept in a specific format)
+HELP_TERMINATOR: str = '</body>' # The string that ends help documentation parsing
+HELP_PRE_START: str = '<h1>' # The string that will start help documentation parsing
+HELP_HEADER_START_TOKEN: str = '<h3>' # The string that will start header string
+HELP_HEADER_END_TOKEN: str = '</h3>' # The string that will end header string
+HELP_TAG_START_TOKEN: str = '<!--' # The string that will start tokens string
+HELP_TAG_END_TOKEN: str = '-->' # The string that will end tokens string
+HELP_PARAGRAPH_START_TOKEN: str = '<p>' # The string that will start paragraph string
+HELP_PARAGRAPH_END_TOKEN: str = '</p>' # The string that will end paragraph string
 
-HELP_QUERY_ACCEPTANCE = 0.7 # Level of tolerance (0.0 to 1.0) while searching help documentation
+HELP_QUERY_ACCEPTANCE: float = 0.7 # Level of tolerance (0.0 to 1.0) while searching help documentation
 
 # The following is the help menu for the program. It handles presenting and searching for program documentation. It loads
 # the contents of the help file upon being intialized. It stores the articles' headers, keywords (for searching), and content.
 # Searching does accept slightly misspelled words.
 
 class HelpMenu(GeneralInterface):
-
+    '''A menu object used for parsing help documentation.'''
     
     def __init__(self):
-        self.name = 'HELP MENU'
-        self.headers = [] # List of help article headers (for displaying the title of the article)
-        self.keywords = [] # List of help article keywords (what is used to search for articles)
-        self.articles = [] # List of article contents
-        self.article_count = 0 # The number of articles loaded into the object
+        self.name: str = 'HELP MENU'
+        self.headers: list[str] = [] # List of help article headers (for displaying the title of the article)
+        self.keywords: list[str] = [] # List of help article keywords (what is used to search for articles)
+        self.articles: list[str] = [] # List of article contents
+        self.article_count: int = 0 # The number of articles loaded into the object
         
         # Open the help file and extract the headers, keywords, and articles
         # The help file is expected to have the following format (breaks are indicated by \n escapes):
         # Header \n Keywords seperated by white spaces \n Article contents \n Line seperator (for easier editing) \n ... \n <END>
         
         # TODO: This is not sustainable (using __file__)
-        help_filename = os.path.join(os.path.dirname(__file__), HELP_FILENAME)
+        help_filename: Any = os.path.join(os.path.dirname(__file__), HELP_FILENAME)
+        documentation: TextIOWrapper
         with open(help_filename, 'r') as documentation:
+
             # Consume head and other starting content
             while HELP_PRE_START not in documentation.readline(): pass
             
             while documentation:
-                new_header = self.parse_file_with_tokens(documentation, HELP_HEADER_START_TOKEN, HELP_HEADER_END_TOKEN)
+                new_header: Optional[str] = self.parse_file_with_tokens(documentation, HELP_HEADER_START_TOKEN, HELP_HEADER_END_TOKEN)
                 
                 if not new_header:
                     break
@@ -56,9 +67,9 @@ class HelpMenu(GeneralInterface):
                 self.article_count += 1
     
     # Helper function for reading the contents of the help documentation
-    def parse_file_with_tokens(self, file, start_token, end_token):
-        result = ''
-        line = file.readline()
+    def parse_file_with_tokens(self, file: TextIOWrapper, start_token: str, end_token: str) -> Optional[str]:
+        result: str = ''
+        line: str = file.readline()
         while line and start_token not in line:
             line = file.readline()
         if not line:
@@ -71,15 +82,18 @@ class HelpMenu(GeneralInterface):
             result += line
             line = file.readline()
         result += line[:line.index(end_token)]
-        # print(f'result: {result}')
         return result
 
-    def remove_tokens_from_string(self, string, start_token, end_token):
-        start_index = string.find(start_token)
-        end_index = string.find(end_token)
+    def remove_tokens_from_string(self, string: str, start_token: str, end_token: str) -> str:
+        # TODO: verify the changes to this method is correct
+        result: str = ''
+        start_index: int = string.find(start_token)
+        end_index: int = string.find(end_token)
         if start_index != -1 and end_index != -1:
             result = self.remove_tokens_from_string(string[:start_index] + string[:end_index+len(end_token)], start_token, end_token)
-        return string
+        return result
+
+        # TODO: HERE IS WERE I STOPPED ADDING ANNOTATIONS:
 
     def parse_input(self, controller, input):
         '''Handle input on behalf of the program.'''
