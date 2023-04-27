0a0df91d23faa21285754c78f115ee9115a08887
jamesacox96 <cox_james4@columbusstate.edu>
2023-04-27 17:11:20 -0400

    added insertion module
    added insertion module. for demo purposes, always choose "no" when asked if youd like to insert into case base

--- src/cbr_excel_output_handler.py
+++ src/cbr_excel_output_handler.py
@@ -1,6 +1,9 @@
 from openpyxl import Workbook, load_workbook
 
-def write_to_file(selected_file, elective_list):
+def write_to_file(selected_file, working_elective_list):
+    electives_list_to_be_added =[]
+    for course in working_elective_list:
+         electives_list_to_be_added.append(course)
     write_count = 0
     wb = load_workbook(selected_file)
     ws = wb.active
@@ -10,10 +13,10 @@ def write_to_file(selected_file, elective_list):
         for j in range (1, 7):
             #get cell value 
             cell_obj = ws.cell(row=i, column=j)
-            if (cell_obj.value == "Course XXXX - Elective (Fa Sp --)" and len(elective_list) > 0):
-                    cell_obj.value = elective_list.pop()
+            if (cell_obj.value == "Course XXXX - Elective (Fa Sp --)" and len(electives_list_to_be_added) > 0):
+                    cell_obj.value = electives_list_to_be_added.pop()
                     write_count += 1
-            elif len(elective_list) == 0:
+            elif len(electives_list_to_be_added) == 0:
                 break
     wb.save(selected_file)
     return "Done - " + str(write_count) + " electives have been scheduled"
