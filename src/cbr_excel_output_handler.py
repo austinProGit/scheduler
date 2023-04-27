66f9080293c65e6d58af93d344d23a968f122032
jamesacox96 <cox_james4@columbusstate.edu>
2023-04-27 12:09:19 -0400

    excel bug fixed

--- src/cbr_excel_output_handler.py
+++ src/cbr_excel_output_handler.py
@@ -10,7 +10,7 @@ def write_to_file(selected_file, elective_list):
         for j in range (1, 7):
             #get cell value 
             cell_obj = ws.cell(row=i, column=j)
-            if (cell_obj.value == "COURSE XXXX - Elective (Fa Sp --)" and len(elective_list) > 0):
+            if (cell_obj.value == "Course XXXX - Elective (Fa Sp --)" and len(elective_list) > 0):
                     cell_obj.value = elective_list.pop()
                     write_count += 1
             elif len(elective_list) == 0:
